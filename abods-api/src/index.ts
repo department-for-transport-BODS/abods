da338479b047367d85cfa9760cf5c82bcb1a275e
Keelan J. <keelan.jenkinson@kpmg.co.uk>
2024-06-12 16:15:00 +0100

    changes session cookie name to prevent conflicts

--- abods-api/src/index.ts
+++ abods-api/src/index.ts
@@ -38,7 +38,7 @@ app.use(
         const cookieHeader = getHeader(event.headers, 'Cookie');
         if (cookieHeader) {
           const cookies = parseCookie(cookieHeader)
-          const sessionid = cookies["sessionid"];
+          const sessionid = cookies["abods_sessionid"];
 
           if(sessionid) {
             const session = await getSession(sessionid, db);
