1d6862389f160b76ab53ce1314abd590e93f64a9
Jonathan Nakandala <Jonathan.Nakandala@kpmg.co.uk>
2024-11-04 17:02:38 +0000

    Use getHeader helper to normalise header fetching

--- abods-api/src/lib/apiauth.ts
+++ abods-api/src/lib/apiauth.ts
@@ -3,7 +3,7 @@ import { SecretsManager } from "@aws-sdk/client-secrets-manager";
 import logger from "../logger.js";
 import { AuthContext, RequestContext } from "../types/extra.js";
 import { GraphQLResolveInfo } from "graphql";
-import { throwUnauthenticatedError } from "../resolvers/helpers.js";
+import { throwUnauthenticatedError, getHeader } from "../resolvers/helpers.js";
 export const hashApiKey = (key: string, hmacSecret: string): string => {
   if (!key || !hmacSecret) {
     throw new Error("Key and HMAC secret are required");
@@ -29,14 +29,12 @@ export const requireApiToken = (context: RequestContext): AuthResult => {
     };
   }
 
-  logger.info(
-    `Authorization Header Present: ${!!context.headers.Authorization}`
-  );
-  const authHeaderData = context.headers.Authorization;
+  const authHeaderData = getHeader(context.headers, "Authorization");
+
   if (!authHeaderData) {
     return {
       isAuthenticated: false,
-      message: "Authorization is missing",
+      message: "Authorization Header is missing is missing",
     };
   }
 
@@ -49,7 +47,7 @@ export const requireApiToken = (context: RequestContext): AuthResult => {
   if (!providedToken) {
     return {
       isAuthenticated: false,
-      message: "API token is required",
+      message: "API token is required in format Bearer <api-key>",
     };
   }
 
