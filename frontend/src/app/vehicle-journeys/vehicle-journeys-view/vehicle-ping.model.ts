010d44a2136a4dd95ef6cd2ceeddc3f7ca39d970
Gareth Somerville <gareth.somerville@doza.consulting>
2024-11-04 10:43:22 +0000

    Fix time not displaying on map pings (#89)

--- frontend/src/app/vehicle-journeys/vehicle-journeys-view/vehicle-ping.model.ts
+++ frontend/src/app/vehicle-journeys/vehicle-journeys-view/vehicle-ping.model.ts
@@ -1,12 +1,10 @@
-import { DateTime } from 'luxon';
 import { AvlPoint, OtpEnum } from '../../../generated/graphql';
 
 export function createVehiclePing(ping: AvlPoint, otp: OtpEnum | null) {
-  const timestamp = DateTime.fromISO(ping.recordedAtTimeUtc);
   return {
     lat: ping.latitude,
     lon: ping.longitude,
-    ts: timestamp,
+    ts: ping.recordedAtTimeUtc,
     onTimePerformance: otp,
     id: ping.latitude.toString() + ping.longitude.toString() + ping.recordedAtTimeUtc,
   };
