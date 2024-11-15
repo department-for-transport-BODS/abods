c4828165f3f864d54aafda8445eecceb2d390c18
Gareth Somerville <gareth.somerville@doza.consulting>
2024-11-15 16:50:53 +0000

    Slightly better

--- abods-api/src/lib/feedMonitoring.ts
+++ abods-api/src/lib/feedMonitoring.ts
@@ -28,27 +28,29 @@ export const getVehicleCounts = (
       expected: number;
       actual: number;
     }[]
-  >(Prisma.sql`SELECT operator_noc                                    AS "operatorId",
-                      CAST(COUNT(*) AS int)                           AS "expected",
-                      CAST(COUNT(CASE WHEN sq.cur THEN 1 END) AS int) AS "actual"
-               FROM (SELECT DISTINCT j.operator_noc,
-                                     j.group_id,
-                                     EXISTS(SELECT 1
-                                            FROM public."SiriVMPositions" s
-                                            WHERE s.date_of_journey = '${start}'
-                                              AND s.group_id = j.group_id
-                                              AND s.operator_ref = j.operator_noc
-                                              AND s.recorded_at_time >= '${start}'
-                                              AND s.recorded_at_time < '${end}') AS cur
-                     FROM public.expected_journeys j
-                     WHERE j.date_of_journey = '${start}'
-                       AND j.expected_journey_start < '${end}'
-                       AND j.expected_journey_end > '${start}'
-                       AND j.operator_noc IS NOT NULL
-                       AND j.group_id IS NOT NULL
-                       AND (CAST(${operatorId} AS text) IS NULL OR j.operator_noc = ${operatorId})
-                       AND j.operator_noc IN (SELECT o.operatorref
-                                              FROM public.bods_organisationoperator o
-                                              WHERE o.organisation_id = ${userOrgId})) sq
-               GROUP BY operator_noc;`);
+  >(Prisma.sql`
+      SELECT operator_noc                                    AS "operatorId",
+             CAST(COUNT(*) AS int)                           AS "expected",
+             CAST(COUNT(CASE WHEN sq.cur THEN 1 END) AS int) AS "actual"
+      FROM (SELECT DISTINCT j.operator_noc,
+                            j.group_id,
+                            EXISTS(SELECT 1
+                                   FROM public."SiriVMPositions" s
+                                   WHERE s.date_of_journey = '${start}'
+                                     AND s.group_id = j.group_id
+                                     AND s.operator_ref = j.operator_noc
+                                     AND s.recorded_at_time >= '${start}'
+                                     AND s.recorded_at_time < '${end}') AS cur
+            FROM public.expected_journeys j
+            WHERE j.date_of_journey = '${start}'
+              AND j.expected_journey_start < '${end}'
+              AND j.expected_journey_end > '${start}'
+              AND j.operator_noc IS NOT NULL
+              AND j.group_id IS NOT NULL
+              AND ((CAST(${operatorId} AS text) IS NULL OR j.operator_noc = ${operatorId}))
+              AND j.operator_noc IN (SELECT o.operatorref
+                                     FROM public.bods_organisationoperator o
+                                     WHERE o.organisation_id = ${userOrgId})) sq
+      GROUP BY operator_noc;
+  `);
 };
