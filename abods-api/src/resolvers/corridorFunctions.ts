1143c0876a6607a90b34b4aacae5af79b34c346a
Karthik Alva <karthik.alva@kpmg.co.uk>
2024-11-19 10:19:05 +0000

    fix for prisma unwrap issue

--- abods-api/src/resolvers/corridorFunctions.ts
+++ abods-api/src/resolvers/corridorFunctions.ts
@@ -393,9 +393,6 @@ export const getStats: CorridorNamespaceResolvers["stats"] = async (
         lt: utcToBstDBInput(toTimestamp),
       },
     },
-    include: {
-      expected_journeys: true,
-    },
   });
 
   const journeyMap: Map<string, Timetable[]> = new Map();
