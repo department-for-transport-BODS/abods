e9aca02f6e66f8419ba885e69146b05aace39ad9
Gareth Somerville <gareth.somerville@doza.consulting>
2024-12-13 10:51:57 +0000

    BODS-7585 journey direction disambiguation (#175)
    * BODS-7570 Choose first matched avl if possible
    
    * BODS-7585 Update database
    
    * BODS-7585 Get direction ref in api
    
    * BODS-7585 Pass direction as a query param and use to filter stops and avls
    
    * Lower case just in case of a potential edge case
    
    * Fix direction on journey nav
    
    * BODS-7608 BODS-7607 Fix getting current journey index
    
    * Fix refreshing stops and avls
    
    * Calculate index once
    
    * Fix query params when going back from last journey
    
    * Different way of filtering avls

--- abods-api/src/resolvers/vehicleJourneyFunctions.ts
+++ abods-api/src/resolvers/vehicleJourneyFunctions.ts
@@ -27,6 +27,7 @@ export const findJourneys: QueryResolvers["findJourneys"] = async (
         expected_journey_start: true,
         group_id: true,
         journey_pattern_description: true,
+        direction: true,
         expected_services: {
           select: {
             line_name: true,
@@ -43,6 +44,7 @@ export const findJourneys: QueryResolvers["findJourneys"] = async (
     .then((j) =>
       j.map((journey) => ({
         groupId: journey.group_id,
+        directionRef: journey.direction,
         startTime: getFormattedDate(journey.expected_journey_start),
         serviceName: journey.journey_pattern_description,
         serviceNumber: journey.expected_services?.line_name ?? "unknown",
@@ -67,6 +69,13 @@ export const getAvls: QueryResolvers["avls"] = async (
     context.db.siriVMPositions
       .findMany({
         where: { group_id: groupId },
+        select: {
+          latitude: true,
+          longitude: true,
+          recorded_at_time: true,
+          vehicle_ref: true,
+          direction_ref: true,
+        },
       })
       .then((j) =>
         j.map((s) => ({
@@ -74,6 +83,7 @@ export const getAvls: QueryResolvers["avls"] = async (
           longitude: s.longitude ?? 0,
           recordedAtTimeUtc: s.recorded_at_time.toISOString(),
           vehicleRef: s.vehicle_ref,
+          directionRef: s.direction_ref ?? "unknown",
         })),
       );
 
@@ -114,6 +124,7 @@ export const getRoute: QueryResolvers["route"] = async (
         common_name: true,
         otp_state: true,
         timestamp_after_estimate: true,
+        direction: true,
       },
     })
     .then((r) =>
@@ -130,6 +141,7 @@ export const getRoute: QueryResolvers["route"] = async (
         stopName: s.common_name ?? "Unknown",
         isTimingPoint: s.is_timing_point ?? false,
         otp: s.otp_state ? OtpEnum[s.otp_state as OtpEnum] : null,
+        directionRef: s.direction ?? "unknown",
       })),
     );
 };
