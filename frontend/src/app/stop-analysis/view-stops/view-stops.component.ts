0f76f120d525d1e4850393c376dc1487753f0257
Karthik Alva <karthik.alva@kpmg.co.uk>
2025-01-14 21:20:45 +0000

    in progress

--- frontend/src/app/stop-analysis/view-stops/view-stops.component.ts
+++ frontend/src/app/stop-analysis/view-stops/view-stops.component.ts
@@ -1,8 +1,22 @@
 import { Component } from "@angular/core";
+import {
+  StopAnalysisListGQL,
+  StopAnalysisType,
+} from "../../../generated/graphql";
+import { map } from "rxjs";
+import { Observable } from "@apollo/client";
 
 @Component({
   selector: "app-view-stops",
   templateUrl: "./view-stops.component.html",
   styleUrls: ["./view-stops.component.scss"],
 })
-export class ViewStopsComponent {}
+export class ViewStopsComponent {
+  constructor(private stopAnalysisQuery: StopAnalysisListGQL) {}
+
+  // stopData(): Observable<StopAnalysisType[]> {
+  //   return this.stopAnalysisQuery
+  //     .fetch({})
+  //     .pipe(map(({ data }) => data?.stopAnalysis as StopAnalysisType[]));
+  // }
+}
