a07f2b4b66c9adc2b5c7d4e13b711240b6ab9baa
Karthik Alva <karthik.alva@kpmg.co.uk>
2025-01-14 13:16:18 +0000

    in progress

---
+++ frontend/src/app/stop-analysis/view-stops/view-stops.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-view-stops",
+  templateUrl: "./view-stops.component.html",
+  styleUrls: ["./view-stops.component.scss"],
+})
+export class ViewStopsComponent {}
