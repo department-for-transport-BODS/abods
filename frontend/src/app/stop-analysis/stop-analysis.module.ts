2765142c1efecf3f0f02f9bd7ed6d08c58b439e1
Karthik Alva <karthik.alva@kpmg.co.uk>
2025-01-22 09:52:44 +0000

    in progress

--- frontend/src/app/stop-analysis/stop-analysis.module.ts
+++ frontend/src/app/stop-analysis/stop-analysis.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from "@angular/common";
 import { StopAnalysisRoutingModule } from "./stop-analysis-routing.module";
 import { ViewStopsComponent } from "./view-stops/view-stops.component";
 import { LayoutModule } from "../layout/layout.module";
+import { StopControlsComponent } from "./stop-controls/stop-controls.component";
 
 @NgModule({
-  declarations: [ViewStopsComponent],
+  declarations: [ViewStopsComponent, StopControlsComponent],
   imports: [CommonModule, LayoutModule, StopAnalysisRoutingModule],
 })
 export class StopAnalysisModule {}
