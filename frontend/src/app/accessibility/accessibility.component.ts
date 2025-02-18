f4888ee780604c019189dfb39625c93a7b24a934
Gareth Somerville <gareth.somerville@doza.consulting>
2025-02-18 09:27:39 +0000

    Back to login link on footer pages (#276)

--- frontend/src/app/accessibility/accessibility.component.ts
+++ frontend/src/app/accessibility/accessibility.component.ts
@@ -1,8 +1,11 @@
 import { Component } from "@angular/core";
+import { LoggedInService } from "../authentication/loggedIn.service";
 
 @Component({
   selector: "app-accessibility",
   templateUrl: "./accessibility.component.html",
   styleUrls: ["./accessibility.component.scss"],
 })
-export class AccessibilityComponent {}
+export class AccessibilityComponent {
+  constructor(public loggedInService: LoggedInService) {}
+}
