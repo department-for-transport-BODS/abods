16a03d9dc0256a8533097eb18cb06412fa9948e1
alvak1 <95031756+alvak1@users.noreply.github.com>
2025-03-21 10:28:49 +0000

    changes to add stop analysis help desk (#381)

--- frontend/src/app/stop-analysis/stop-analysis-routing.module.ts
+++ frontend/src/app/stop-analysis/stop-analysis-routing.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { AuthGuardService } from "../authentication/auth-guard.service";
 import { StopAnalysisComponent } from "./stop-analysis.component";
+import { HelpdeskResolver } from "../shared/resolvers/helpdesk.resolver";
 
 const routes: Routes = [
   {
     path: "",
     canActivateChild: [AuthGuardService],
     children: [{ path: "", component: StopAnalysisComponent }],
+    data: {
+      helpdeskFolder: "stopAnalysis",
+      helpdeskTitle: "Stop analysis",
+    },
+    resolve: { helpdesk: HelpdeskResolver },
   },
 ];
 
