37a84806559741822d21f4af88778c7cf7e83729
Karthik Alva <karthik.alva@kpmg.co.uk>
2025-05-21 16:39:05 +0100

    changes as per review comments

--- frontend/src/app/on-time/on-time-grid/on-time-grid.component.ts
+++ frontend/src/app/on-time/on-time-grid/on-time-grid.component.ts
@@ -216,7 +216,7 @@ export class OnTimeGridComponent<TData extends AbstractPerformance> {
     }),
   );
 
-  directions: Direction[] = [];
+  directions: Direction[] = [Direction.Inbound];
   private _columnDescriptions: ColumnDescription[] = [];
   @Input()
   get columnDescriptions() {
