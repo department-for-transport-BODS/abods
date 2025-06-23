fc8b7f5467059d7203b8bd2dd141ab02cde00878
Andy Crow <andy.crow@doza.consulting>
2025-06-23 15:44:38 +0200

    Add getServicePatternDistanceGeom to VehicleJourneysSearchService

--- frontend/src/app/vehicle-journeys/vehicle-journeys-search/vehicle-journeys-search.service.ts
+++ frontend/src/app/vehicle-journeys/vehicle-journeys-search/vehicle-journeys-search.service.ts
@@ -1,12 +1,19 @@
 import { Injectable } from "@angular/core";
-import { Journey, JourneysGQL } from "../../../generated/graphql";
+import {
+  Journey,
+  JourneysGQL,
+  ServicePatternDistanceGeomGQL,
+} from "../../../generated/graphql";
 import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
 import { DateTime } from "luxon";
 
 @Injectable({ providedIn: "root" })
 export class VehicleJourneysSearchService {
-  constructor(private journeysGQL: JourneysGQL) {}
+  constructor(
+    private journeysGQL: JourneysGQL,
+    private servicePatternDistanceGeomGQL: ServicePatternDistanceGeomGQL,
+  ) {}
 
   fetchDayJourneys(
     dateOfJourney: string,
@@ -20,5 +27,18 @@ export class VehicleJourneysSearchService {
           .sort((a, b) => a.startTime.localeCompare(b.startTime));
       }),
     );
+  }
+
+  getServicePatternDistanceGeom(
+    vehicleJourneyId: string,
+  ): Observable<{ distance: number; geom: [number, number][] }> {
+    return this.servicePatternDistanceGeomGQL
+      .fetch({ vehicleJourneyId }, { fetchPolicy: "no-cache" })
+      .pipe(
+        map((result) => ({
+          distance: result.data.getServicePatternDistanceGeom.distance,
+          geom: result.data.getServicePatternDistanceGeom.geom,
+        })),
+      );
   }
 }
