9538bbb31277cf950cdbeb8aed9ea3eb19a2eb3c
Andy Crow <andy.crow@doza.consulting>
2025-06-23 15:56:28 +0200

    Simplify dropdown component width + fix issue with overflowing text

--- frontend/src/app/shared/components/dropdown/dropdown.component.ts
+++ frontend/src/app/shared/components/dropdown/dropdown.component.ts
@@ -37,10 +37,6 @@ export class DropdownComponent implements OnInit {
     return this.width + "px";
   }
 
-  get contentWidth(): string {
-    return this.width - dropdownPadding * 2 - dropdownBorder * 2 + "px";
-  }
-
   get isOpen(): boolean {
     return (
       this.ngxTippyService.getInstance(this.identifier)?.state.isMounted ??
