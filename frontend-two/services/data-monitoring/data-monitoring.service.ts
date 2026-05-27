b881cd47b3be33954b323fc2b3da6b35de7f1f0b
Deanna <deanna.sharma@kainos.com>
2026-05-27 13:51:08 +0100

    refactor data-monitoring page to use graphql client

--- frontend-two/services/data-monitoring/data-monitoring.service.ts
+++ frontend-two/services/data-monitoring/data-monitoring.service.ts
@@ -1,16 +1,18 @@
-import { graphqlRequest } from "@/services/api";
-import { EmbeddedUrlResponse } from "@/types/data-monitoring";
-import { EMBEDDED_URL_QUERY } from "@/services/data-monitoring/data-monitoring.operations";
+import { apolloClient } from "@/services/apolloClient";
+import {
+  AwsQuicksightUser,
+  DashboadEmbeddedUrlDocument,
+  DashboadEmbeddedUrlQuery,
+} from "../../src/generated/graphql";
 
 export const dataMonitoringService = {
-  fetchEmbeddedUrl: async (
-    apiUrl: string,
-  ): Promise<EmbeddedUrlResponse | null> => {
+  fetchEmbeddedUrl: async (): Promise<AwsQuicksightUser | null> => {
     try {
-      const result = await graphqlRequest<{
-        embeddedUrl: EmbeddedUrlResponse;
-      }>(apiUrl, EMBEDDED_URL_QUERY);
-      return result.embeddedUrl ?? null;
+      const result = await apolloClient.query<DashboadEmbeddedUrlQuery>({
+        query: DashboadEmbeddedUrlDocument,
+      });
+
+      return result.data?.embeddedUrl ?? null;
     } catch (error) {
       console.warn("Failed to fetch embedded URL:", error);
       return null;
